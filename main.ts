44739760629d44bd354b9e04fc081bf4c356ae57
John A Stevenson <volcan01010@users.noreply.github.com>
2024-12-05 17:23:38 +0000

    Add code start round, detect shake and be sad

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.StickFigure)
+    while (true) {
+        if (input.acceleration(Dimension.Strength) - 1000 > 100) {
+            basic.showIcon(IconNames.Sad)
+            music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+            break;
+        }
+    }
 })
